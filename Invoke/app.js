3e334bb7722407f3430c76b9b93f00ecdccce343
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-08-24 20:06:18 +0530

    Worked with temp, top_p, stop and more

--- Invoke/app.js
+++ Invoke/app.js
@@ -7,17 +7,27 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 async function main() {
   const completion = await groq.chat.completions
     .create({
+      temperature: 1,
+      top_p: 0.2,
+      stop: "ga", // Negative -> Ne
+      max_completion_tokens: 1000,
+      frequency_penalty: 1,
+      presence_penalty: 1,
       messages: [
         {
             role: "system",
-            content: "You are Jarvis, a smart personal assistant."
+            content: `You are Jarvis, a smart review grader. Your task id to analyze given 
+            review and return the sentiment. Classify the review as positive, neutral or 
+            negative. Output must be in single word`
         },
         {
           role: "user",
-          content: "Who are you?",
+          content: `Review: These headphones arrives  quickly and look great, but the left earcup stopped working 
+                    after a week.
+                    Sentiment:`,
         },
       ],
-      model: "openai/gpt-oss-20b",
+      model: "llama-3.3-70b-versatile",
     })
     .then((chatCompletion) => {
       console.log(chatCompletion.choices[0]?.message?.content || "");
