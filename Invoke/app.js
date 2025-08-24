2d71441107cfc6919f8ab8c8135dfd9d9009d2fe
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-08-24 22:09:37 +0530

    OpenAI JSON Schema

--- Invoke/app.js
+++ Invoke/app.js
@@ -7,18 +7,34 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 async function main() {
   const completion = await groq.chat.completions
     .create({
-      temperature: 1,
-      top_p: 0.2,
-      stop: "ga", // Negative -> Ne
-      max_completion_tokens: 1000,
-      frequency_penalty: 1,
-      presence_penalty: 1,
+      //   temperature: 1,
+      //   top_p: 0.2,
+      //   stop: "ga", // Negative -> Ne
+      //   max_completion_tokens: 1000,
+      //   frequency_penalty: 1,
+      //   presence_penalty: 1,
+      response_format: { type: "json_object" },
       messages: [
         {
-            role: "system",
-            content: `You are Jarvis, a smart review grader. Your task id to analyze given 
-            review and return the sentiment. Classify the review as positive, neutral or 
-            negative. Output must be in single word`
+          role: "system",
+          content: `You are a interview grader assistant. Your task is to generate candidate evaluation score.
+            Ouput must be following JSON structure:
+            {
+                "confidence": number (1-10 scale),
+                "accuracy": number (1-10 scale),
+                "pass": boolean (true or false)
+            }
+                The response must: 
+                   1. Include ALL fields shown above
+                   2. Use only the exact field names shown
+                   3. Follow the exact data types specified
+                   4.Contain ONLY the JSON object and nothing else
+            `,
+          // content: `You are Jarvis, a smart review grader. Your task id to analyze given
+          // review and return the sentiment. Classify the review as positive, neutral or
+          // negative. You must return the result in valid JSON structure.
+          // example: {"Sentiment": "Negative"}
+          // `
         },
         {
           role: "user",
